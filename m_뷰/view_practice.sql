69fec2a86a87bbdd267e58f4f0ca8ff7f8952296
devgiants75 <devgiants75@naver.com>
2025-08-04 10:49:43 +0900

    20250804 dcl

--- m_뷰/view_practice.sql
+++ m_뷰/view_practice.sql
@@ -10,19 +10,38 @@ USE `school`;
 # last_name: 문자열(50),
 # age: 정수
 # major: 문자열(50)
-
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    major VARCHAR(50)
+);
 
 -- courses 테이블 생성 --
 # cours_id: 정수, 기본키
 # course_name: 문자열(100)
 # instructor: 문자열(100)
 # credit_hours: 정수
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100),
+    instructor VARCHAR(100),
+    credit_hours INT
+);
 
 -- student_courses 테이블 --
 # student_id: 정수
 # course_id: 정수
 # >> 위의 두 컬럼을 복합키로 설정(기본키)
 # >> 각 컬럼은 students, courses 테이블에서 참조
+create table student_courses (
+    student_id int,
+    course_id int,
+    primary key (student_id, course_id),
+    foreign key (student_id) references students(student_id),
+    foreign key (course_id) references courses(course_id)
+);
 
 # cf) 각 테이블에 데이터 삽입 4개 이상 
 INSERT INTO students (student_id, first_name, last_name, age, major)
@@ -57,3 +76,19 @@ VALUES (1, 101),
 4. 뷰가 정상적으로 생성되었는지 확인(조회)
 */
 
+DROP VIEW IF EXISTS student_course_view;
+
+CREATE VIEW student_course_view
+AS
+	SELECT
+		concat(S.first_name, S.last_name) as 'student name'
+        , C.course_name
+        , C.instructor
+    FROM
+		student_courses SC
+			INNER JOIN students S 
+				ON SC.student_id = S.student_id
+            INNER JOIN courses C
+				ON SC.course_id = C.course_id; 
+
+SELECT * FROM student_course_view;
---
+++ n_dcl/control01.sql
@@ -0,0 +1,37 @@
+### n_dcl >>> control01 ###
+
+### SQL 종류 ###
+# 1. DDL (Definition): 데이터 정의 언어 (데이터베이스, 데이터베이스 개체 구조 정의) 
+# 		- CREATE, ALTER, DROP, TRUNCATE
+# 2. DML (Manipulation): 데이터 조작 언어 (데이터 CRUD) 
+#		- INSERT, SELECT, UPDATE, DELETE
+# 3. DCL (Control): 데이터 통제 언어 
+#		- GRANT(승인하다), REVOKE(취소하다)
+
+### DCL(Data Control Language) ###
+# : 데이터베이스 사용자 권한을 제어하는 SQL 명령어
+# - 보안과 접근 제어를 위한 역할 수행
+
+# 1. GRANT
+# : 특정 사용자에게 권한을 부여
+
+# 2. REVOKE
+# : 특정 사용자에게 부여된 권한을 회수 
+
+### DCL 사용 목적 ###
+# 1. 보안 강화: 민감한 데이터에 접근 가능한 사용자 제한 가능
+# 2. 데이터 무결성 유지: 특정 사용자만 데이터를 수정할 수 있도록 제한 가능
+# 3. 역할 기반 접근 제어: 사용자의 역할에 따라 필요한 권한만 부여 가능 
+
+
+
+
+
+
+
+
+
+
+
+
+
